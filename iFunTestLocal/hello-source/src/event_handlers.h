33998b1abefdb26241be57111ab4cef40735ea1f
staeho <staeho@momocorp.co.kr>
2015-09-22 21:04:37 +0900

    first

---
+++ iFunTestLocal/hello-source/src/event_handlers.h
@@ -0,0 +1,20 @@
+// PLEASE ADD YOUR EVENT HANDLER DECLARATIONS HERE.
+
+#ifndef SRC_EVENT_HANDLERS_H_
+#define SRC_EVENT_HANDLERS_H_
+
+#include <funapi.h>
+
+#include "hello_messages.pb.h"
+#include "hello_object.h"
+#include "hello_rpc_messages.pb.h"
+#include "hello_types.h"
+
+
+namespace hello {
+
+void RegisterEventHandlers();
+
+}  // namespace hello
+
+#endif  // SRC_EVENT_HANDLERS_H_
---
+++ iFunTestLocal/hello-source/src/hello_types.h
@@ -0,0 +1,16 @@
+#ifndef SRC_HELLO_TYPES_H_
+#define SRC_HELLO_TYPES_H_
+
+#include <funapi.h>
+
+
+namespace hello {
+
+using fun::string;
+using fun::shared_ptr;
+using fun::weak_ptr;
+using fun::Uuid;
+
+}  // namespace hello
+
+#endif  // SRC_HELLO_TYPES_H_
